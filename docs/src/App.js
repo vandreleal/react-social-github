bae5f8e630719bf3eb18310e1627d3b453836192
Gustavo Sampaio <gbritosampaio@gmail.com>
2017-06-25 04:49:34 -0300

    Tooltip options

--- docs/src/App.js
+++ docs/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import ReactMarkdown from 'react-markdown';
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +15,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <ReactMarkdown source={'# h1'} />,
+
       </div>
     );
   }
