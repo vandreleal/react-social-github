25a1afec0c5ac7197ba343d535ce754c4fb935be
Vandré Leal Cândido <vleal@uvsouthsourcing.com>
2017-06-25 00:35:42 -0300

    Test Rearranged

--- testrehub/src/App.js
+++ testrehub/src/App.js
@@ -9,12 +9,10 @@ class App extends Component {
         <h1>Re<em>Hub</em></h1>
 
         <Github org="facebook" type="tooltip"></Github>
+        <Github org="facebook" type="tooltip" tooltipOnHover={false}></Github>
 
         <Github org="facebook" type="tooltip" tooltipOnHover={true} fab={true} iconColor="red" iconWidth={64} iconHeight={64}></Github>
         <Github org="facebook" type="tooltip" tooltipOnHover={true} fab={true} fabCorner="bottom-left" iconColor="orange" iconWidth={48} iconHeight={48}></Github>
-        <Github org="facebook" type="tooltip" tooltipOnHover={false}></Github>
-
-        <Github org="facebook" type="tooltip" iconColor="orange" iconWidth="96" iconHeight="96"></Github>
 
         {/*<Github user="facebook" repo="react" type="widget"></Github>*/}
 
