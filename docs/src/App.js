ede12cef8f2d03a2c5ff976dcfba4eea556dc843
Vandré Leal Cândido <vleal@uvsouthsourcing.com>
2017-06-25 05:59:06 -0300

    Test

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import { Github } from 'react-social-github';
 import logo from './logo.svg';
 import './App.css';
 
 import ReactMarkdown from 'react-markdown';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: 'facebook',
+      repo: 'react'
+    };
+
+    this.handleChangeUser = this.handleChangeUser.bind(this);
+    this.handleChangeRepo = this.handleChangeRepo.bind(this);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeUser(event) {
+    this.setState({user: event.target.value});
+  }
+
+  handleChangeRepo(event) {
+    this.setState({repo: event.target.value});
+  }
+
+  handleSubmit(event) {
+    return (
+      <Github user={this.state.user} repo={this.state.repo}></Github>
+    )
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="App">
@@ -16,7 +45,23 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
 
-        <ReactMarkdown source={'# h1'} />,
+        <ReactMarkdown source={'# h1'} />
+
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={this.state.user} onChange={this.handleChangeUser} />
+          </label>
+
+          <label>
+            Repo:
+            <input type="text" value={this.state.repo} onChange={this.handleChangeRepo} />
+          </label>
+
+          <input type="submit" value="Submit" />
+        </form>
+
+        <Github user={this.state.user} repo={this.state.repo}></Github>
 
       </div>
     );
