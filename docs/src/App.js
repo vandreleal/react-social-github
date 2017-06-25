7b4939beaa84338644e7f24ac031313055dd6a17
Vandré Leal Cândido <vleal@uvsouthsourcing.com>
2017-06-25 06:40:47 -0300

    Test

--- docs/src/App.js
+++ docs/src/App.js
@@ -11,15 +11,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      markdown: '# Loading',
-      user: 'facebook',
-      repo: 'react'
+      markdown: '# Loading'
     };
-
-    this.handleChangeUser = this.handleChangeUser.bind(this);
-    this.handleChangeRepo = this.handleChangeRepo.bind(this);
-
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentWillMount() {
@@ -44,43 +37,12 @@ class App extends Component {
     });
   }
 
-  handleChangeUser(event) {
-    this.setState({user: event.target.value});
-  }
-
-  handleChangeRepo(event) {
-    this.setState({repo: event.target.value});
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    return (
-      <Github user={this.state.user} repo={this.state.repo}></Github>
-    )
-  }
-
   render() {
     return (
       <div className="app">
-        {/*}<form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input type="text" value={this.state.user} onChange={this.handleChangeUser} />
-          </label>
-
-          <label>
-            Repo:
-            <input type="text" value={this.state.repo} onChange={this.handleChangeRepo} />
-          </label>
-
-          <input type="submit" value="Submit" />
-        </form>*/}
-
         <ReactMarkdown source={this.state.markdown} className="markdown-body" />
 
-        <Github user="vandreleal"></Github>
-        <Github user="facebook" repo="react"></Github>
-
+        <Github user="facebook" repo="react" type="button" tooltipOnHover={true} fab={true}></Github>
       </div>
     );
   }
