84526a4bff863b9455538e9ede5503d8d211bbf7
Vandré Leal Cândido <vleal@uvsouthsourcing.com>
2017-06-24 23:22:14 -0300

    New Type

--- testrehub/src/App.js
+++ testrehub/src/App.js
@@ -8,13 +8,13 @@ class App extends Component {
       <div className="App">
         <h1>Re<em>Hub</em></h1>
 
-        <Github org="facebook" presentation="tooltip" tooltipOnHover={true}></Github>
+        <Github org="facebook" type="tooltip" tooltipOnHover={true}></Github>
 
-        {/*<Github user="facebook" repo="react" presentation="widget"></Github>*/}
+        {/*<Github user="facebook" repo="react" type="widget"></Github>*/}
 
-        <Github user="vandreleal" presentation="widget"></Github>
+        <Github user="vandreleal" type="widget"></Github>
 
-        <Github user="dungahk" text="Github" presentation="link"></Github>
+        <Github user="dungahk" text="Profile" type="link"></Github>
       </div>
     );
   }
